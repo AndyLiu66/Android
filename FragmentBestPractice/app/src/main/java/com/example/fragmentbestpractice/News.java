b647ccb61f62187bfc4d44547f36231a23aa9fa5
Andy <andy66@lonlife.cn>
2017-07-03 15:18:20 +0800

    FragmentBestPractice

---
+++ FragmentBestPractice/app/src/main/java/com/example/fragmentbestpractice/News.java
@@ -0,0 +1,56 @@
+//package com.example.fragmentbestpractice;
+//
+///**
+// * Created by Administrator on 2017/7/1 0001.
+// */
+//
+//public class News {
+//
+//    private String title;
+//    private String content;
+//
+//    public void setContent(String content) {
+//        this.content = content;
+//    }
+//
+//    public void setTitle(String title) {
+//
+//        this.title = title;
+//    }
+//
+//    public String getContent() {
+//
+//        return content;
+//    }
+//
+//    public String getTitle() {
+//
+//        return title;
+//    }
+//}
+
+package com.example.fragmentbestpractice;
+
+public class News {
+
+    private String title;
+
+    private String content;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+}
